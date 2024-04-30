993d34efe0e8cfcebcebfc3f1a16df055b94a426
jacrifla <jaquelini_cris@hotmail.com>
2024-04-29 21:35:54 -0300

    mudanca de algumas cores no darktheme

--- lib/theme.dart
+++ lib/theme.dart
@@ -11,7 +11,7 @@ class AppTheme {
         error: Color(0xFFD11111), // Cor de erro
         background: Color(0xFFE1E1E1), // Cor de fundo
         onPrimary: Color(0xFFF5F5F5), // Cor do texto dos botões
-        onSecondary: Color(0xFF202123), // Cor do texto
+        onSecondary: Color(0xFF446D9D), // Cor do texto
       ),
       appBarTheme: const AppBarTheme(
         backgroundColor: Colors.white, // Cor de fundo do AppBar
@@ -25,22 +25,22 @@ class AppTheme {
   static ThemeData darkTheme() {
     return ThemeData(
       fontFamily: 'montserrat',
-      primaryColor: const Color(0xFF2196F3), // Cor primária
+      primaryColor: const Color(0xFF001F3E), // Cor primária
       colorScheme: const ColorScheme.dark(
-        primary: Color(0xFF001F3E), // Cor primária
-        secondary: Color(0xFF64B5F6), // Cor secundária
-        error: Color.fromARGB(255, 252, 46, 46), // Cor de erro
-        background: Color.fromARGB(255, 52, 52, 52), // Cor de fundo
-        onPrimary: Color.fromARGB(255, 0, 0, 0), // Cor do texto dos botões
-        onSecondary: Color(0xFFFFFFFF), // Cor do texto
+        primary: Color(0xFF64B5F6), // Cor primária equivalente ao tema light
+        secondary:
+            Color(0xFF64B5F6), // Cor secundária equivalente ao tema light
+        error: Color(0xFFEF9A9A), // Cor de erro equivalente ao tema light
+        background: Color(0xFF121212), // Cor de fundo
+        onPrimary: Color(0xFF000000), // Cor do texto dos botões
+        onSecondary: Color(0xFFF5F5F5), // Cor do texto
       ),
       appBarTheme: const AppBarTheme(
-        backgroundColor: Color(0xFF121212), // Cor de fundo do AppBar
+        backgroundColor: Colors.black, // Cor de fundo do AppBar
         titleTextStyle:
             TextStyle(color: Colors.white), // Cor do texto do AppBar
-        iconTheme: IconThemeData(color: Color(0xFFF5F5F5)),
       ),
-      iconTheme: const IconThemeData(color: Color(0xFFF5F5F5)),
+      iconTheme: const IconThemeData(color: Color(0xFF001F3E)),
     );
   }
 }
