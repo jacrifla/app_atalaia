ee29bae20b95a7bca522cf1d6612ac02410c0227
jacrifla <jaquelini_cris@hotmail.com>
2024-05-03 16:53:25 -0300

    colocado navegacao

--- lib/widgets/menu.dart
+++ lib/widgets/menu.dart
@@ -1,65 +1,134 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:app_atalaia/pages/perfil_page.dart';
+import 'package:app_atalaia/pages/switch_page.dart';
 import 'package:flutter/material.dart';
+
+import '../pages/grupo_page.dart';
 
 class CustomDrawer extends StatelessWidget {
   const CustomDrawer({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Drawer(
-      child: ListView(
-        padding: EdgeInsets.symmetric(vertical: 40),
-        children: <Widget>[
-          ListTile(
-            leading: Icon(Icons.tune),
-            title: Text('Grupos'),
-            onTap: () {},
-          ),
-          ListTile(
-            leading: Icon(Icons.lightbulb),
-            title: Text('Pontos'),
-            onTap: () {},
-          ),
-          ListTile(
-            leading: Icon(Icons.bar_chart),
-            title: Text('Monitoramento'),
-            onTap: () {},
-          ),
-          ListTile(
-            leading: Icon(Icons.person),
-            title: Text('Perfil'),
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => PerfilPage(),
+    final screenHeight = MediaQuery.of(context).size.height;
+
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: <Widget>[
+        SizedBox(height: 82), // Espaçamento no topo do Drawer
+        Container(
+          width: MediaQuery.of(context).size.width * 0.6,
+          height: screenHeight * 0.6,
+          color: Theme.of(context).colorScheme.background,
+          child: Column(
+            children: <Widget>[
+              ListTile(
+                title: Text(
+                  'Menu',
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    color: Theme.of(context).colorScheme.primary,
+                  ),
                 ),
-              );
-            },
-          ),
-          ListTile(
-            leading: Icon(Icons.question_mark_outlined),
-            title: Text('Ajuda'),
-            onTap: () {},
-          ),
-          ListTile(
-            tileColor: Theme.of(context).colorScheme.onSecondary,
-            leading: Icon(
-              Icons.logout,
-              color: Theme.of(context).colorScheme.onPrimary,
-            ),
-            title: Text(
-              'Sair',
-              style: TextStyle(
-                color: Theme.of(context).colorScheme.onPrimary,
               ),
-            ),
-            onTap: () {},
+              SizedBox(height: 20),
+              ItemMenu(
+                label: 'Grupos',
+                icon: Icon(Icons.tune),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => GroupPage(),
+                    ),
+                  );
+                },
+              ),
+              ItemMenu(
+                label: 'Pontos',
+                icon: Icon(Icons.lightbulb),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => SwitchPage(),
+                    ),
+                  );
+                },
+              ),
+              // ItemMenu(
+              //   label: 'Monitoramento',
+              //   icon: Icon(Icons.bar_chart),
+              //   height: itemHeight,
+              // ),
+              ItemMenu(
+                label: 'Perfil',
+                icon: Icon(Icons.person),
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => PerfilPage(),
+                    ),
+                  );
+                },
+              ),
+              // ItemMenu(
+              //   label: 'Ajuda',
+              //   icon: Icon(Icons.question_mark_outlined),
+              //   height: itemHeight,
+              // ),
+              Container(
+                color: Theme.of(context).colorScheme.onSecondary,
+                child: ItemMenu(
+                  label: 'Sair',
+                  colorText: Theme.of(context).colorScheme.background,
+                  icon: Icon(
+                    Icons.logout,
+                    color: Theme.of(context).colorScheme.background,
+                  ),
+                  onTap: () {},
+                ),
+              ),
+            ],
           ),
-        ],
+        ),
+      ],
+    );
+  }
+}
+
+class ItemMenu extends StatelessWidget {
+  final String label;
+  final Icon icon;
+  final VoidCallback? onTap;
+  final Color? tileColor;
+  final Color? colorText;
+
+  const ItemMenu({
+    super.key,
+    required this.label,
+    required this.icon,
+    this.tileColor,
+    this.colorText,
+    this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      tileColor: tileColor,
+      leading: icon,
+      iconColor: Theme.of(context).colorScheme.primary,
+      title: Text(
+        label,
+        style: TextStyle(
+          color: colorText,
+        ),
       ),
+      onTap: onTap,
     );
   }
 }
