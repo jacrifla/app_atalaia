5d8e8b12dfaad9eb7c992ef5e2fa75acf4041c93
jacrifla <jaquelini_cris@hotmail.com>
2024-05-04 10:46:46 -0300

    add menu

--- lib/pages/switch_page.dart
+++ lib/pages/switch_page.dart
@@ -1,5 +1,9 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:app_atalaia/widgets/header.dart';
 import 'package:flutter/material.dart';
+
+import '../widgets/menu.dart';
 
 class SwitchPage extends StatefulWidget {
   const SwitchPage({super.key});
@@ -12,6 +16,7 @@ class _SwitchPageState extends State<SwitchPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      drawer: CustomDrawer(),
       appBar: Header(title: 'Pontos'),
       body: Center(
         child: Text('Pontos'),
