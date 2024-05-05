9b41bd133d3833e0d277052624438b5fd42b67a8
Sthefani Souza <slauraic@hotmail.com>
2024-05-05 11:02:01 -0300

    Atualizando rotas

--- api2/core/Routes.php
+++ api2/core/Routes.php
@@ -2,9 +2,9 @@
 
 require_once 'Http.php';
 
-Http::get('/produto', 'ProdutoController@show');
-Http::get('/produto/find/{id}', 'ProdutoController@find');
-Http::post('/produto/add', 'ProdutoController@add');
-Http::post('/produto/add-json', 'ProdutoController@addJson');
-Http::put('/produto/edit', 'ProdutoController@edit');
-Http::delete('/produto/delete/{id}', 'ProdutoController@delete');
+// Http::get('/switch', 'SwitchController@getSwitches');
+// Http::get('/switchgroup', 'GroupSwitchController@getGroups');
+Http::post('/login', 'LoginController@login');
+Http::post('/register', 'RegisterController@createUser');
+// Http::put('/produto/edit', 'ProdutoController@edit');
+// Http::delete('/produto/delete/{id}', 'ProdutoController@delete');
