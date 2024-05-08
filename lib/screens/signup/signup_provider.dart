d9cf9a2991807480e311206287ed48a79dbb91b0
jacrifla <jaquelini_cris@hotmail.com>
2024-05-08 16:45:35 -0300

    add errors

--- lib/screens/signup/signup_provider.dart
+++ lib/screens/signup/signup_provider.dart
@@ -1,5 +1,7 @@
 import 'package:dio/dio.dart';
 
+import '../../utils/config.dart';
+
 class SignupProvider {
   final Dio dio = Dio();
 
@@ -12,7 +14,7 @@ class SignupProvider {
       String name, String email, String phone, String password) async {
     try {
       final response = await dio.post(
-        'http://192.168.137.144:80/app_atalaia/api2/register',
+        '${Config.apiUrl}register',
         data: {
           'name': name,
           'email': email,
@@ -24,17 +26,17 @@ class SignupProvider {
       if (response.statusCode == 201) {
         return {'status': 'success', 'data': response.data};
       } else {
-        return {'status': 'error', 'message': response.data['msg']};
+        throw Exception('Failed to create user: ${response.data['msg']}');
       }
     } catch (e) {
       if (e is DioError) {
         if (e.response != null) {
-          return {'status': 'error', 'message': e.response!.data['msg']};
+          throw Exception('Failed to create user: ${e.response!.data['msg']}');
         } else {
-          return {'status': 'error', 'message': 'Falha de conexão.'};
+          throw Exception('Failed to connect to the server.');
         }
       } else {
-        return {'status': 'error', 'message': 'Erro desconhecido.'};
+        throw Exception('Unknown error occurred.');
       }
     }
   }
