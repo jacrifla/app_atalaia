d7d04cca4af7e4df4910f929ab359bd5d5bc69b4
jacrifla <jaquelini_cris@hotmail.com>
2024-05-08 16:38:43 -0300

    caminho difrerente

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,5 @@
 // ignore_for_file: prefer_const_constructors
-import 'package:app_atalaia/guard/guard_screen.dart';
+import 'package:app_atalaia/screens/guard/guard_screen.dart';
 import 'package:flutter/material.dart';
 
 import 'screens/help_screen.dart';
