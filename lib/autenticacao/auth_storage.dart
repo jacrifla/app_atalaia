861556eb0519e45d7740147d995926a1e3b9e4c4
jacrifla <jaquelini_cris@hotmail.com>
2024-05-08 16:28:14 -0300

    get_storage

---
+++ lib/autenticacao/auth_storage.dart
@@ -0,0 +1,22 @@
+import 'package:get_storage/get_storage.dart';
+
+class AuthStorage {
+  static final _box = GetStorage();
+
+  static Future<void> saveUserId(String userId) async {
+    if (userId == null) {
+      print('ta null essa porra');
+      return;
+    }
+    print('UUID recebido para salvar: $userId');
+    await _box.write('userId', userId);
+  }
+
+  static String? getUserId() {
+    return _box.read('userId');
+  }
+
+  static Future<String?> readUuid() async {
+    return getUserId();
+  }
+}
