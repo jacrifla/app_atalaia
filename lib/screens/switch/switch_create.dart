e62f004c4057ee53e61f2ab230b806e0ae512ebb
jacrifla <jaquelini_cris@hotmail.com>
2024-05-08 16:45:52 -0300

    deixado tela estatica

--- lib/screens/switch/switch_create.dart
+++ lib/screens/switch/switch_create.dart
@@ -1,28 +1,29 @@
-// ignore_for_file: use_build_context_synchronously
-
 import 'package:flutter/material.dart';
+
 import '../../widgets/menu.dart';
 import '../../widgets/build_input.dart';
 import '../../widgets/button_icon.dart';
 import '../../widgets/header.dart';
-import 'switch_controller.dart';
+// import '../error_screen.dart';
+import '../success_screen.dart';
 
 class SwitchCreateScreen extends StatelessWidget {
   final TextEditingController nameController = TextEditingController();
   final TextEditingController macController = TextEditingController();
   final TextEditingController wattsController = TextEditingController();
 
-  SwitchCreateScreen({super.key});
+  SwitchCreateScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: const Header(title: 'Adicionar Ponto'),
-      endDrawer: const MenuDrawer(),
+      appBar: Header(title: 'Adicionar Ponto'),
+      endDrawer: MenuDrawer(),
       body: Padding(
         padding: const EdgeInsets.all(20.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.end,
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: [
             Text(
               'Nomeie o novo ponto e escolha entre os opções a seguir',
@@ -33,48 +34,44 @@ class SwitchCreateScreen extends StatelessWidget {
                 fontWeight: FontWeight.bold,
               ),
             ),
-            BuildInput(
-              labelText: 'Nome',
-              controller: nameController,
-              icon: const Icon(Icons.polyline),
-            ),
-            BuildInput(
-              labelText: 'Endereço MAC',
-              controller: macController,
-              icon: const Icon(Icons.network_ping),
-            ),
-            BuildInput(
-              labelText: 'Watts',
-              controller: wattsController,
-              keyboardType: TextInputType.number,
-              icon: const Icon(Icons.flash_on),
+            Column(
+              children: [
+                BuildInput(
+                  labelText: 'Nome',
+                  controller: nameController,
+                  icon: const Icon(Icons.polyline),
+                ),
+                BuildInput(
+                  labelText: 'Endereço MAC',
+                  controller: macController,
+                  icon: const Icon(Icons.network_ping),
+                ),
+                BuildInput(
+                  labelText: 'Watts',
+                  controller: wattsController,
+                  keyboardType: TextInputType.number,
+                  icon: const Icon(Icons.flash_on),
+                ),
+              ],
             ),
             const SizedBox(height: 20),
             ButtonIcon(
               labelText: 'Adicionar',
-              onPressed: () async {
-                String name = nameController.text;
-                String macAddress = macController.text;
-                String watts = wattsController.text;
+              onPressed: () {
+                // String name = nameController.text;
+                // String macAddress = macController.text;
+                // int watts = int.tryParse(wattsController.text) ?? 0;
 
-                bool success = await SwitchController.createSwitch(
-                  name: name,
-                  macAddress: macAddress,
-                  watts: watts,
-                );
+                // Simulação de sucesso
+                bool success = true;
 
                 if (success) {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    const SnackBar(
-                      content: Text('Ponto adicionado com sucesso'),
-                      backgroundColor: Colors.green,
-                    ),
-                  );
-                } else {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    const SnackBar(
-                      content: Text('Falha ao adicionar ponto'),
-                      backgroundColor: Colors.red,
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const SuccessScreen(
+                        message: 'Ponto adicionado com sucesso',
+                      ),
                     ),
                   );
                 }
