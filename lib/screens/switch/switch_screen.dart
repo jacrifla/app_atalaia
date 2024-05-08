d830932511e745d33bc933cf5b1eca4e8d8c7d71
jacrifla <jaquelini_cris@hotmail.com>
2024-05-08 16:30:27 -0300

    add pontos fakes

--- lib/screens/switch/switch_screen.dart
+++ lib/screens/switch/switch_screen.dart
@@ -4,18 +4,74 @@ import '../../widgets/button_icon.dart';
 import '../../widgets/header.dart';
 import '../../widgets/menu.dart';
 import 'switch_create.dart';
+import 'switch_model.dart'; // Importe o modelo SwitchModel
 
 class SwitchScreen extends StatelessWidget {
   const SwitchScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    // Lista de pontos fictícios
+    final List<SwitchModel> switches = [
+      SwitchModel(
+          name: 'Switch 1',
+          macAddress: '00:11:22:33:44:55',
+          watts: 100,
+          isActive: true),
+      SwitchModel(
+          name: 'Switch 2',
+          macAddress: '11:22:33:44:55:66',
+          watts: 150,
+          isActive: false),
+      SwitchModel(
+          name: 'Switch 3',
+          macAddress: '22:33:44:55:66:77',
+          watts: 200,
+          isActive: true),
+    ];
+
     return Scaffold(
       appBar: const Header(title: 'Pontos'),
       endDrawer: const MenuDrawer(),
-      body: const SingleChildScrollView(
-        child: Column(
-          children: [],
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: ListView.builder(
+          itemCount: switches.length,
+          itemBuilder: (context, index) {
+            final switchModel = switches[index];
+            return Card(
+              color: switchModel.isActive
+                  ? Theme.of(context).colorScheme.onSecondary
+                  : Theme.of(context).colorScheme.primary,
+              child: Padding(
+                padding: const EdgeInsets.all(16),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Expanded(
+                      child: Text(
+                        switchModel.name,
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Theme.of(context).colorScheme.background,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        // Exluir
+                      },
+                      child: const Icon(
+                        Icons.close,
+                        color: Colors.red,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            );
+          },
         ),
       ),
       floatingActionButton: Column(
