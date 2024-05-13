32fd27800b37360f3872db586278adb7d011e215
jacrifla <jaquelini_cris@hotmail.com>
2024-05-13 20:27:14 -0300

    tirado comentarios

--- lib/screens/login/login_controller.dart
+++ lib/screens/login/login_controller.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: use_build_context_synchronously
+// ignore_for_file: use_build_context_synchronously, avoid_print
 import 'package:flutter/material.dart';
 
 import '../../utils/auth_provider.dart';
@@ -9,19 +9,19 @@ class LoginController {
   final LoginProvider _loginProvider;
   final AuthProvider _authProvider;
 
-  LoginController(this._loginProvider, this._authProvider);
+  LoginController(
+    this._loginProvider,
+    this._authProvider,
+  );
 
   Future<void> loginUser(
       BuildContext context, String email, String password) async {
     try {
-      // Chama o método de login do LoginProvider
       await _loginProvider.login(email, password);
       print('UUID após o login: ${_authProvider.userId}');
 
-      // Depois de logar com sucesso, vai para a tela principal
       Navigator.pushNamed(context, '/home');
     } catch (error) {
-      // Em caso de erro, vai exibir uma mensagem de erro
       await Navigator.push(
         context,
         MaterialPageRoute(
--- lib/utils/auth_provider.dart
+++ lib/utils/auth_provider.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:get_storage/get_storage.dart';
 
 class AuthProvider extends ChangeNotifier {
-  // Criando uma instancia
   final box = GetStorage();
 
   String? get userId => box.read('uuid');
