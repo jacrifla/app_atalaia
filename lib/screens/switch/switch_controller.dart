bc627607930f9cdc38461be29e7d3a0c9c806602
jacrifla <jaquelini_cris@hotmail.com>
2024-05-13 20:36:34 -0300

    add outros metodos, nao testado

--- lib/screens/switch/switch_controller.dart
+++ lib/screens/switch/switch_controller.dart
@@ -1,22 +1,61 @@
-import 'package:app_atalaia/utils/config.dart';
-import 'package:dio/dio.dart';
+import 'switch_model.dart';
+import 'switch_provider.dart';
 
 class SwitchController {
-  final Dio _dio = Dio();
+  final SwitchProvider _switchProvider = SwitchProvider();
 
-  Future<bool> createSwitch(Map<String, dynamic> data) async {
+  Future<List<dynamic>> fetchSwitches(String userId) async {
     try {
-      final response = await _dio.post(
-        '${Config.apiUrl}/switch/create',
-        data: data,
-      );
+      return await _switchProvider.fetchSwitches(userId);
+    } catch (e) {
+      print('CONTROLLER');
+      throw Exception('erro no controller : $e');
+    }
+  }
 
-      if (response.statusCode == 200) {
-        return true;
-      } else {
-        return false;
-      }
-    } catch (error) {
+  Future<bool> createSwitch(
+      String name, String watts, String macAddress, String userId) async {
+    try {
+      final result =
+          await _switchProvider.createSwitch(name, watts, macAddress, userId);
+
+      return result['status'] == 'success';
+    } catch (e) {
+      return false;
+    }
+  }
+
+  Future<List<SwitchModel>> getSwitches(String userId) async {
+    try {
+      return await _switchProvider.getSwitches(userId);
+    } catch (e) {
+      throw Exception('Erro ao obter switches: $e');
+    }
+  }
+
+  Future<bool> updateSwitch(Map<String, dynamic> data) async {
+    try {
+      final result = await _switchProvider.updateSwitch(data);
+      return result['status'] == 'success';
+    } catch (e) {
+      return false;
+    }
+  }
+
+  Future<bool> deleteSwitch(String macAddress) async {
+    try {
+      final result = await _switchProvider.deleteSwitch(macAddress);
+      return result['status'] == 'success';
+    } catch (e) {
+      return false;
+    }
+  }
+
+  Future<bool> toggleSwitch(String macAddress) async {
+    try {
+      final result = await _switchProvider.toggleSwitch(macAddress);
+      return result['status'] == 'success';
+    } catch (e) {
       return false;
     }
   }
--- lib/screens/switch/switch_provider.dart
+++ lib/screens/switch/switch_provider.dart
@@ -1,22 +1,40 @@
-import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+
+import 'package:dio/dio.dart';
 import '../../utils/config.dart';
+import 'switch_model.dart';
 
 class SwitchProvider extends ChangeNotifier {
   final Dio _dio = Dio();
 
-  initDio() {
-    _dio.options.connectTimeout = const Duration(seconds: 5);
-    _dio.options.receiveTimeout = const Duration(seconds: 3);
+  Future<List<dynamic>> fetchSwitches(String userId) async {
+    print('USER ID PROVIDER: $userId');
+    try {
+      final response =
+          await _dio.get('${Config.apiUrl}switches', data: {'user_id': userId}
+              // queryParameters: {'user_id': userId},
+              );
+      print('STATUS CODE: ${response.statusCode}');
+      if (response.statusCode == 200) {
+        print('entrou?');
+        final data = response.data['dados'];
+        // return {'status': 'success', 'data': response.data['dados']};
+
+        // return data.map((json) => SwitchModel.fromJson(json)).toList();
+        return data;
+      } else {
+        throw Exception('Failed to fetch switches');
+      }
+    } on Exception catch (error) {
+      throw Exception('ERRO PROVIDER: $error');
+    }
   }
 
   Future<Map<String, dynamic>> createSwitch(
       String name, String watts, String macAddress, String userId) async {
-    print(
-        'PROVIDER - Name: $name Watts: $watts MAC Address: $macAddress User ID: $userId');
     try {
       final response = await _dio.post(
-        '${Config.apiUrl}/switches/new',
+        '${Config.apiUrl}switches/new',
         data: {
           'name': name,
           'watts': watts,
@@ -25,31 +43,125 @@ class SwitchProvider extends ChangeNotifier {
         },
       );
 
+      return _handleResponse(response);
+    } catch (error) {
+      return _handleError(error);
+    }
+  }
+
+  Future<List<SwitchModel>> getSwitches(String userId) async {
+    try {
+      final response = await _dio.get(
+        '${Config.apiUrl}switches',
+        queryParameters: {'user_id': userId},
+      );
+
+      if (response.statusCode == 200) {
+        List<SwitchModel> switches = [];
+        for (var switchData in response.data['dados']) {
+          switches.add(SwitchModel.fromJson(switchData));
+        }
+        return switches;
+      } else if (response.statusCode == 400) {
+        throw Exception('Erro interno: ${response.data['msg']}');
+      } else if (response.statusCode == 500) {
+        throw Exception('Erro no servidor: ${response.data['msg']}');
+      } else {
+        throw Exception(
+            'Erro desconhecido. Status code: ${response.statusCode}');
+      }
+    } catch (error) {
+      throw Exception('Erro ao conectar ao servidor: $error');
+    }
+  }
+
+  Future<Map<String, dynamic>> getSwitch(String macAddress) async {
+    try {
+      final response = await _dio.post(
+        '${Config.apiUrl}switches/getone',
+        queryParameters: {'mac_address': macAddress},
+      );
+
       if (response.statusCode == 200) {
         return {'status': 'success', 'data': response.data};
       } else if (response.statusCode == 400) {
-        throw Exception('Failed to create switch: ${response.data['msg']}');
-      } else if (response.statusCode == 401) {
-        throw Exception('Unauthorized: ${response.data['msg']}');
-      } else if (response.statusCode == 404) {
-        throw Exception('Not found: ${response.data['msg']}');
+        throw Exception('Erro interno: ${response.data['msg']}');
       } else if (response.statusCode == 500) {
-        throw Exception('Internal server error: ${response.data['msg']}');
+        throw Exception('Erro no servidor: ${response.data['msg']}');
       } else {
         throw Exception(
-            'Unknown error occurred. Status code: ${response.statusCode}');
+            'Erro desconhecido. Status code: ${response.statusCode}');
       }
-    } catch (e) {
-      if (e is DioError) {
-        if (e.response != null) {
-          throw Exception(
-              'Failed to create switch: ${e.response!.data['msg']}');
-        } else {
-          throw Exception('Failed to connect to the server.');
-        }
-      } else {
-        throw Exception('Unknown error occurred: $e');
-      }
+    } catch (error) {
+      throw Exception('Erro ao conectar ao servidor: $error');
     }
+  }
+
+  Future<Map<String, dynamic>> updateSwitch(Map<String, dynamic> data) async {
+    try {
+      final response = await _dio.post(
+        '${Config.apiUrl}switches/update',
+        data: data,
+      );
+
+      return _handleResponse(response);
+    } catch (error) {
+      return _handleError(error);
+    }
+  }
+
+  Future<Map<String, dynamic>> deleteSwitch(String macAddress) async {
+    try {
+      final response = await _dio.put(
+        '${Config.apiUrl}switches/delete',
+        data: {'mac_address': macAddress},
+      );
+
+      if (response.statusCode == 200) {
+        return _handleResponse(response);
+      } else if (response.statusCode == 400) {
+        throw Exception('Erro interno: ${response.data['msg']}');
+      } else if (response.statusCode == 500) {
+        throw Exception('Erro no servidor: ${response.data['msg']}');
+      } else {
+        throw Exception(
+            'Erro desconhecido. Status code: ${response.statusCode}');
+      }
+    } catch (error) {
+      return _handleError(error);
+    }
+  }
+
+  Future<Map<String, dynamic>> toggleSwitch(String macAddress) async {
+    try {
+      final response = await _dio.post(
+        '${Config.apiUrl}switches/toggle',
+        data: {'mac_address': macAddress},
+      );
+
+      return _handleResponse(response);
+    } catch (error) {
+      return _handleError(error);
+    }
+  }
+
+  Map<String, dynamic> _handleResponse(Response response) {
+    if (response.statusCode == 200) {
+      return {'status': 'success', 'data': response.data};
+    } else {
+      throw Exception('Erro ao processar a requisição: ${response.data}');
+    }
+  }
+
+  List<Map<String, dynamic>> _handleResponseList(Response response) {
+    if (response.statusCode == 200) {
+      return List<Map<String, dynamic>>.from(response.data['data']);
+    } else {
+      throw Exception('Erro ao processar a requisição: ${response.data}');
+    }
+  }
+
+  Map<String, dynamic> _handleError(error) {
+    throw Exception('Erro ao processar a requisição: $error');
   }
 }
