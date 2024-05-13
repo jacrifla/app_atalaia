250bfc8c3fd1f61430f123d664aacffb338c6cfc
jacrifla <jaquelini_cris@hotmail.com>
2024-05-13 20:25:49 -0300

    ajustado com todos os dados que tem em dados na api

--- lib/screens/switch/switch_model.dart
+++ lib/screens/switch/switch_model.dart
@@ -1,31 +1,67 @@
 class SwitchModel {
-  final String name;
+  final String? id;
+  final String uuid;
+  final String? userId;
+  final String? iconName;
+  final String? groupId;
   final String macAddress;
-  final int watts;
+  final String name;
   final bool isActive;
+  final String? lastTimeActive;
+  final int watts;
+  final bool keepActive;
+  final bool scheduleActive;
+  final String? scheduleLastActivation;
+  final String? scheduleStart;
+  final String? scheduleEnd;
+  final String createdAt;
+  final String? updatedAt;
+  final String? deletedAt;
+  final bool guardActive;
 
   SwitchModel({
-    required this.name,
+    this.id,
+    required this.uuid,
+    required this.userId,
+    this.iconName,
+    this.groupId,
     required this.macAddress,
-    required this.watts,
+    required this.name,
     required this.isActive,
+    this.lastTimeActive,
+    required this.watts,
+    required this.keepActive,
+    required this.scheduleActive,
+    this.scheduleLastActivation,
+    this.scheduleStart,
+    this.scheduleEnd,
+    required this.createdAt,
+    this.updatedAt,
+    this.deletedAt,
+    required this.guardActive,
   });
 
   factory SwitchModel.fromJson(Map<String, dynamic> json) {
     return SwitchModel(
-      name: json['name'],
+      id: json['id'],
+      uuid: json['uuid'],
+      userId: json['user_id'],
+      iconName: json['icon_name'],
+      groupId: json['group_id'],
       macAddress: json['mac_address'],
-      watts: json['watts'],
-      isActive: json['is_active'],
+      name: json['name'],
+      isActive: json['is_active'] == "1",
+      lastTimeActive: json['last_time_active'],
+      watts: int.parse(json['watts']),
+      keepActive: json['keep_active'] == "1",
+      scheduleActive: json['schedule_active'] == "1",
+      scheduleLastActivation: json['schedule_last_activation'],
+      scheduleStart: json['schedule_start'],
+      scheduleEnd: json['schedule_end'],
+      createdAt: json['created_at'],
+      updatedAt: json['updated_at'],
+      deletedAt: json['deleted_at'],
+      guardActive: json['guard_active'] == "1",
     );
-  }
-
-  Map<String, dynamic> toJson() {
-    return {
-      'name': name,
-      'mac_address': macAddress,
-      'watts': watts,
-      'is_active': isActive,
-    };
   }
 }
