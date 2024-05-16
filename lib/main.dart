9f6ac883b2bb83fee3270e9c6bb2e6f3f5cb7920
jacrifla <jaquelini_cris@hotmail.com>
2024-05-16 15:06:27 -0300

    acrescentado outro changenotifier

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,10 @@ import 'package:get_storage/get_storage.dart';
 import 'package:provider/provider.dart';
 import 'app.dart';
 import 'screens/switch/switch_provider.dart';
+import 'screens/switch/switch_controller.dart';
 import 'utils/auth_provider.dart';
 
-void main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await GetStorage.init();
   runApp(
@@ -13,6 +14,8 @@ void main() async {
       providers: [
         ChangeNotifierProvider<AuthProvider>(create: (_) => AuthProvider()),
         ChangeNotifierProvider<SwitchProvider>(create: (_) => SwitchProvider()),
+        ChangeNotifierProvider<SwitchController>(
+            create: (_) => SwitchController()),
       ],
       child: const MyApp(),
     ),
