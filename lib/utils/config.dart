985044e917c9d0223f28ea4252b7cbdb1871a330
jacrifla <jaquelini_cris@hotmail.com>
2024-05-18 18:38:13 -0300

    add ip note

--- lib/utils/config.dart
+++ lib/utils/config.dart
@@ -1,3 +1,7 @@
 class Config {
-  static const String apiUrl = 'http://192.168.1.25:80/app_atalaia/api2/';
+  // PC
+  // static const String apiUrl = 'http://192.168.1.25:80/app_atalaia/api2/';
+
+  // NOTE
+  static const String apiUrl = 'http://192.168.1.29:80/app_atalaia/api2/';
 }
