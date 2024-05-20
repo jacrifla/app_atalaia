142c8c25c48f2bcb21e60fcb1ea47f2c418ac6c1
jacrifla <jaquelini_cris@hotmail.com>
2024-05-20 19:58:43 -0300

    separado topicos da ajuda

---
+++ lib/screens/help/topics.dart
@@ -0,0 +1,27 @@
+final List<Map<String, String>> topics = [
+  {
+    'title': 'Como criar conta',
+    'content':
+        'Para criar uma conta, vá para a tela de registro e preencha seus detalhes.',
+  },
+  {
+    'title': 'Configurando para o primeiro uso',
+    'content':
+        'Após criar uma conta, você será guiado por um assistente de configuração para configurar o aplicativo para o primeiro uso.',
+  },
+  {
+    'title': 'Perfil',
+    'content':
+        'No seu perfil, você pode visualizar e editar suas informações pessoais e preferências.',
+  },
+  {
+    'title': 'Grupos',
+    'content':
+        'Os Presets são um sistema que permite ao usuário comandar diversos Pontos de Iluminação por vez. Eles podem ser acionados de diversas maneiras: Através de um comando feito pelo usuário; Através de um horário e/ou dia da semana pré-estabelecido; Comando de emergência (no caso de Presets de Guarda).',
+  },
+  {
+    'title': 'Pontos',
+    'content':
+        'Para adicionar um novo ponto, vá para a tela "Criar Ponto". Insira um nome único para o ponto no campo "Nome do Ponto" e o endereço MAC do dispositivo associado ao ponto no campo "MAC Address". Em seguida, clique no botão de marca de verificação para salvar as informações do ponto.',
+  },
+];
