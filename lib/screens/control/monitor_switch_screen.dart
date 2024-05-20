0c7e35ac4b2827903a83f55fb4c8fb6ba8f2b84e
jacrifla <jaquelini_cris@hotmail.com>
2024-05-20 20:29:02 -0300

    iniciada tela

--- lib/screens/control/monitor_switch_screen.dart
+++ lib/screens/control/monitor_switch_screen.dart
@@ -1,1 +1,15 @@
+import 'package:flutter/material.dart';
 
+class MonitorSwitchScreen extends StatefulWidget {
+  const MonitorSwitchScreen({super.key});
+
+  @override
+  State<MonitorSwitchScreen> createState() => _MonitorSwitchScreenState();
+}
+
+class _MonitorSwitchScreenState extends State<MonitorSwitchScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
