3e2defa3ca78411c3e8ed6bfa94d658d5b9c5799
jacrifla <jaquelini_cris@hotmail.com>
2024-05-20 19:22:27 -0300

    add SingleChildScrollView para nao dar estouro

--- lib/screens/success_screen.dart
+++ lib/screens/success_screen.dart
@@ -20,38 +20,42 @@ class SuccessScreen extends StatelessWidget {
       body: Center(
         child: Padding(
           padding: const EdgeInsets.all(30.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Icon(
-                Icons.check,
-                size: 150,
-                color: Theme.of(context).colorScheme.primary,
-              ),
-              const SizedBox(height: 40),
-              Text(
-                message,
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Icon(
+                  Icons.check_circle_outline_rounded,
+                  size: 250,
+                  color: Theme.of(context).colorScheme.primary,
                 ),
-              ),
-              const SizedBox(height: 60),
-              ButtonIcon(
-                onPressed: () {
-                  if (alternativeRoute != null) {
-                    Navigator.pushNamed(context, alternativeRoute!);
-                  } else {
-                    Navigator.popUntil(
-                      context,
-                      ModalRoute.withName(screen!),
-                    );
-                  }
-                },
-                icon: Icon(Icons.check),
-              ),
-            ],
+                const SizedBox(height: 40),
+                Text(
+                  message,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(height: 120),
+                ButtonIcon(
+                  height: 50,
+                  onPressed: () {
+                    if (alternativeRoute != null) {
+                      Navigator.pushNamed(context, alternativeRoute!);
+                    } else {
+                      Navigator.popUntil(
+                        context,
+                        ModalRoute.withName(screen!),
+                      );
+                    }
+                  },
+                  labelText: 'Voltar',
+                ),
+              ],
+            ),
           ),
         ),
       ),
