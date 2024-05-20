81704c99415592b3867bc0f79d6fd8d7ad13dd82
jacrifla <jaquelini_cris@hotmail.com>
2024-05-20 20:21:44 -0300

    algumas alteracoes

--- lib/screens/group_switch/group_screen.dart
+++ lib/screens/group_switch/group_screen.dart
@@ -1,10 +1,13 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: library_private_types_in_public_api
+
 import 'package:flutter/material.dart';
 
 import '../../widgets/menu.dart';
 import '../../widgets/button_icon.dart';
 import '../../widgets/header.dart';
 import 'create_group_screen.dart';
+import 'group_card_delete.dart';
+import 'group_model.dart';
 
 class GroupScreen extends StatefulWidget {
   const GroupScreen({super.key});
@@ -14,8 +17,9 @@ class GroupScreen extends StatefulWidget {
 }
 
 class _GroupScreenState extends State<GroupScreen> {
-  List<GroupInfo> userGroups = [
-    GroupInfo(
+  List<GroupModel> userGroups = [
+    GroupModel(
+      groupId: '1',
       groupName: 'Grupo 1',
       groupIcon: Icons.group,
       groupCommon: true,
@@ -25,8 +29,10 @@ class _GroupScreenState extends State<GroupScreen> {
       autoActivationTime: false,
       activationStartTime: '',
       activationEndTime: '',
+      isActive: true,
     ),
-    GroupInfo(
+    GroupModel(
+      groupId: '2',
       groupName: 'Grupo 2',
       groupIcon: Icons.home,
       groupCommon: false,
@@ -36,30 +42,21 @@ class _GroupScreenState extends State<GroupScreen> {
       autoActivationTime: true,
       activationStartTime: '10h',
       activationEndTime: '12h',
+      isActive: false,
     ),
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: Header(title: 'Grupos'),
-      endDrawer: MenuDrawer(),
+      appBar: const Header(title: 'Grupos'),
+      endDrawer: const MenuDrawer(),
       body: Padding(
         padding: const EdgeInsets.all(20.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            ButtonIcon(
-              icon: Icon(Icons.add),
-              labelText: 'Criar Novo Grupo',
-              onPressed: () async {
-                await Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => CreateGroupScreen()),
-                );
-              },
-            ),
-            SizedBox(height: 20),
+            const SizedBox(height: 20),
             Expanded(
               child: ListView.builder(
                 itemCount: userGroups.length,
@@ -72,64 +69,15 @@ class _GroupScreenState extends State<GroupScreen> {
           ],
         ),
       ),
-    );
-  }
-}
-
-class GroupInfo {
-  final String groupName;
-  final IconData groupIcon;
-  final bool groupCommon;
-  final bool randomTime;
-  final bool keepActive;
-  final int keepActiveHours;
-  final bool autoActivationTime;
-  final String activationStartTime;
-  final String activationEndTime;
-
-  GroupInfo({
-    required this.groupName,
-    required this.groupIcon,
-    required this.groupCommon,
-    required this.randomTime,
-    required this.keepActive,
-    required this.keepActiveHours,
-    required this.autoActivationTime,
-    required this.activationStartTime,
-    required this.activationEndTime,
-  });
-}
-
-class GroupCard extends StatelessWidget {
-  final GroupInfo groupInfo;
-
-  const GroupCard({super.key, required this.groupInfo});
-
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      margin: EdgeInsets.symmetric(vertical: 10),
-      color: Theme.of(context).colorScheme.onSecondary,
-      child: ListTile(
-        onTap: () {},
-        leading: Icon(
-          groupInfo.groupIcon,
-          color: Theme.of(context).colorScheme.onPrimary,
-        ),
-        title: Text(
-          groupInfo.groupName,
-          style: TextStyle(
-            color: Theme.of(context).colorScheme.onPrimary,
-            fontWeight: FontWeight.w600,
-          ),
-        ),
-        trailing: IconButton(
-          icon: Icon(
-            Icons.clear,
-            color: Theme.of(context).colorScheme.error,
-          ),
-          onPressed: () {},
-        ),
+      floatingActionButton: ButtonIcon(
+        icon: const Icon(Icons.add),
+        labelText: 'Criar Novo Grupo',
+        onPressed: () async {
+          await Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => const CreateGroupScreen()),
+          );
+        },
       ),
     );
   }
