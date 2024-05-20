c0fa6b7467e9c9577a0080c2521f58dc9497e2b8
jacrifla <jaquelini_cris@hotmail.com>
2024-05-20 20:31:48 -0300

    usando no pc

--- lib/utils/config.dart
+++ lib/utils/config.dart
@@ -1,9 +1,9 @@
 class Config {
   // PC
-  // static const String apiUrl = 'http://192.168.1.25:80/app_atalaia/api2/';
+  static const String apiUrl = 'http://192.168.1.25:80/app_atalaia/api2/';
 
   // NOTE
-  static const String apiUrl = 'http://192.168.1.29:80/app_atalaia/api2/';
+  // static const String apiUrl = 'http://192.168.1.29:80/app_atalaia/api2/';
 
   // REMOTE SERVER (TEMP)
   // static const String apiUrl = 'https://atalaiaproject.000webhostapp.com/';
