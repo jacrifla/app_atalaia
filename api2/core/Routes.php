5ad6adfd7867b18d55b126621d11870be1586bad
Sthefani Souza <slauraic@hotmail.com>
2024-05-21 17:18:14 -0300

    Criando rotas para grupo, nao ativas ainda

--- api2/core/Routes.php
+++ api2/core/Routes.php
@@ -6,8 +6,8 @@ Http::post('/login', 'LoginController@login');
 Http::post('/register', 'RegisterController@createUser');
 Http::put('/user/update', 'UserController@updateUser');
 Http::put('/user/delete', 'UserController@deleteUser');
+
 // Switches
-// Http::get('/switchgroup', 'GroupSwitchController@getGroups');
 Http::post('/switches', 'SwitchController@getSwitches');
 Http::post('/switches/getone', 'SwitchController@getSwitch');
 Http::post('/switches/new', 'SwitchController@createSwitch');
@@ -15,7 +15,15 @@ Http::put('/switches/edit', 'SwitchController@updateSwitch');
 Http::post('/switches/toggle', 'SwitchController@toggleSwitch');
 Http::put('/switches/delete', 'SwitchController@deleteSwitch');
 
+//Groups
+// Http::post('/groups', 'GroupSwitchController@getGroups');
+// Http::post('/groups/getone', 'GroupSwitchController@getGroup');
+// Http::post('/groups/new', 'GroupSwitchController@createGroup');
+// Http::post('/groups/edit', 'GroupSwitchController@updateGroup');
+// Http::post('/groups/toggle', 'GroupSwitchController@toggleGroup');
+// Http::post('/groups/delete', 'GroupSwitchController@deleteGroup');
+
 //Rotas internas
 Http::post('/macAddress', 'MacAddressController@insert');
 Http::put('/macAddress/edit', 'MacAddressController@update');
-Http::delete('/macAddress/detele', 'MacAddressController@delete');
+Http::delete('/macAddress/delete', 'MacAddressController@delete');
