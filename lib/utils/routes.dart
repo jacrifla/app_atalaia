e98457941f9233090f72652deb996f2d5b4d22ab
jacrifla <jaquelini_cris@hotmail.com>
2024-05-31 01:28:25 -0300

    alterando para o arquivo certo

--- lib/utils/routes.dart
+++ lib/utils/routes.dart
@@ -1,5 +1,6 @@
 import 'package:app_atalaia/view/monitor_switch_screen.dart';
 import 'package:flutter/material.dart';
+import '../view/group_create_screen.dart';
 import '../view/group_screen.dart';
 import '../view/guard_screen.dart';
 import '../view/help_screen.dart';
@@ -13,22 +14,45 @@ import '../view/login_screen.dart';
 import '../view/perfil_screen.dart';
 import '../view/signup_screen.dart';
 
+import 'package:flutter/material.dart';
+
 class AppRoutes {
+  static const String home = '/home';
+  static const String login = '/';
+  static const String signUp = '/register';
+  static const String userProfile = '/profile';
+  static const String help = '/help';
+  static const String loading = '/loading';
+  static const String groupScreen = '/group_screen';
+  static const String groupCreate = '/group_create';
+  static const String groupEdit = '/group_edit';
+  static const String switchScreen = '/switch_screen';
+  static const String switchCreate = '/switch_create';
+  static const String switchEdit = '/switch_edit';
+  static const String guard = '/guard';
+  static const String errorScreen = '/error_screen';
+  static const String successScreen = '/success_screen';
+  static const String confirmScreen = '/confirm_screen';
+  static const String recover = '/recover';
+
   static Map<String, WidgetBuilder> getRoutes() {
     return {
-      '/': (context) => const LoginScreen(),
-      '/group_switch': (context) => const GroupScreen(),
-      '/help': (context) => const HelpScreen(),
-      '/home': (context) => const HomeScreen(),
-      '/perfil': (context) => const PerfilScreen(),
-      '/recover': (context) => const RecoverScreen(),
-      '/recover_confirmation': (context) => const RecoverConfirmationScreen(),
-      '/signup': (context) => const SignupScreen(),
-      '/switch': (context) => const SwitchScreen(),
-      '/switch_create': (context) => SwitchCreateScreen(),
-      '/guard': (context) => GuardManagementScreen(),
-      '/loading': (context) => const LoadingScreen(),
-      '/monitor': (context) => const MonitorSwitchScreen()
+      home: (context) => const HomeScreen(),
+      login: (context) => const LoginScreen(),
+      signUp: (context) => const SignupScreen(),
+      help: (context) => const HelpScreen(),
+      userProfile: (context) => const PerfilScreen(),
+      loading: (context) => const LoadingScreen(),
+      groupScreen: (context) => const GroupScreen(),
+      groupCreate: (context) => const CreateGroupScreen(),
+      // groupEdit: (context) => EditGroupScreen(),
+      switchScreen: (context) => const SwitchScreen(),
+      switchCreate: (context) => SwitchCreateScreen(),
+      // switchEdit: (context) => EditSwitchScreen(),
+      guard: (context) => GuardManagementScreen(),
+      //   errorScreen: (context) => const ErrorScreen(),
+      //   successScreen: (context) => const SuccessScreen(),
+      //   confirmScreen: (context) => const ConfirmationScreen(),
     };
   }
 }
