fe88feac295b5a8fc6b91e36f32005863a490102
jacrifla <jaquelini_cris@hotmail.com>
2024-06-01 15:32:41 -0300

    refatorado

--- lib/view/group_screen.dart
+++ lib/view/group_screen.dart
@@ -1,101 +1,127 @@
-// ignore_for_file: library_private_types_in_public_api
-
 import 'package:flutter/material.dart';
-import '../widgets/menu.dart';
+import 'package:provider/provider.dart';
+
+import '../utils/auth_provider.dart';
 import '../widgets/button_icon.dart';
 import '../widgets/header.dart';
+import '../widgets/menu.dart';
+import '../controller/group_controller.dart';
 import 'group_create_screen.dart';
-import '../widgets/group_card_actions.dart';
 import '../model/group_model.dart';
+import '../widgets/group_card_actions.dart';
 
 class GroupScreen extends StatefulWidget {
   const GroupScreen({super.key});
 
   @override
-  _GroupScreenState createState() => _GroupScreenState();
+  State<GroupScreen> createState() => _GroupScreenState();
 }
 
 class _GroupScreenState extends State<GroupScreen> {
-  List<GroupModel> userGroups = [
-    GroupModel(
-      groupId: '1',
-      groupName: 'Grupo 1',
-      groupIcon: Icons.group,
-      groupCommon: true,
-      randomTime: false,
-      keepActive: false,
-      keepActiveHours: 0,
-      autoActivationTime: false,
-      activationStartTime: '',
-      activationEndTime: '',
-      isActive: true,
-    ),
-    GroupModel(
-      groupId: '2',
-      groupName: 'Grupo 2',
-      groupIcon: Icons.home,
-      groupCommon: false,
-      randomTime: true,
-      keepActive: true,
-      keepActiveHours: 2,
-      autoActivationTime: true,
-      activationStartTime: '10h',
-      activationEndTime: '12h',
-      isActive: false,
-    ),
-    GroupModel(
-      groupId: '2',
-      groupName: 'Grupo de Toda a Casa',
-      groupIcon: Icons.abc,
-      groupCommon: false,
-      randomTime: true,
-      keepActive: true,
-      keepActiveHours: 2,
-      autoActivationTime: true,
-      activationStartTime: '10h',
-      activationEndTime: '12h',
-      isActive: false,
-    ),
-  ];
+  late Future<List<GroupModel>> _groupsFuture;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadGroups();
+  }
+
+  void _loadGroups() {
+    final authProvider = Provider.of<AuthProvider>(context, listen: false);
+    final userId = authProvider.userId;
+    if (userId != null) {
+      _groupsFuture = GroupController().getGroups(context);
+    } else {
+      _groupsFuture = Future.error('User ID is null');
+    }
+  }
+
+  void _refreshGroups() {
+    setState(() {
+      _loadGroups();
+    });
+  }
+
+  void _navigateToCreateGroup() {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => const CreateGroupScreen()),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
+    final authProvider = Provider.of<AuthProvider>(context);
+    final userId = authProvider.userId;
+
+    if (userId == null) {
+      return const Scaffold(
+        body: Center(
+          child: Text('Erro: usuário não autenticado'),
+        ),
+      );
+    }
+
     return Scaffold(
       appBar: const Header(title: 'Grupos'),
       endDrawer: const MenuDrawer(),
       body: Padding(
         padding: const EdgeInsets.all(20.0),
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            const SizedBox(height: 20),
             Expanded(
-              child: ListView.builder(
-                itemCount: userGroups.length,
-                itemBuilder: (context, index) {
-                  final group = userGroups[index];
-                  return GroupCardActions(groupInfo: group);
+              child: FutureBuilder<List<GroupModel>>(
+                future: _groupsFuture,
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  } else if (snapshot.hasError) {
+                    return Center(
+                      child: Text('Erro ao carregar grupos: ${snapshot.error}'),
+                    );
+                  } else if (snapshot.hasData && snapshot.data!.isNotEmpty) {
+                    final groups = snapshot.data!;
+                    return ListView.builder(
+                      itemCount: groups.length,
+                      itemBuilder: (context, index) {
+                        final group = groups[index];
+                        return GroupCardActions(groupInfo: group);
+                      },
+                    );
+                  } else {
+                    return const Center(
+                      child: Text('Nenhum grupo encontrado.'),
+                    );
+                  }
                 },
               ),
             ),
           ],
         ),
       ),
-      floatingActionButton: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20.0),
-        child: ButtonIcon(
-          icon: const Icon(Icons.add),
-          labelText: 'Criar Novo Grupo',
-          onPressed: () async {
-            await Navigator.push(
-              context,
-              MaterialPageRoute(
-                  builder: (context) => const CreateGroupScreen()),
-            );
-          },
-        ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 8.0),
+            child: ButtonIcon(
+              labelText: 'Criar Novo Grupo',
+              onPressed: _navigateToCreateGroup,
+              icon: const Icon(Icons.add),
+              backgroundColor: Theme.of(context).colorScheme.primary,
+            ),
+          ),
+          ButtonIcon(
+            labelText: 'Atualizar Grupos',
+            onPressed: _refreshGroups,
+            icon: const Icon(Icons.refresh),
+            backgroundColor: Theme.of(context).colorScheme.onSecondary,
+          ),
+        ],
       ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
