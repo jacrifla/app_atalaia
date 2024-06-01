6a650f414ccef538a473f30fe5036a2b1ec85b7a
jacrifla <jaquelini_cris@hotmail.com>
2024-06-01 15:27:22 -0300

    refatorado

--- lib/controller/group_controller.dart
+++ lib/controller/group_controller.dart
@@ -2,270 +2,74 @@ import 'package:flutter/material.dart';
 import '../model/group_model.dart';
 import '../model/switch_model.dart';
 import '../provider/group_provider.dart';
+import '../utils/auth_provider.dart';
 
 class GroupController extends ChangeNotifier {
   final GroupProvider _groupProvider = GroupProvider();
+  final AuthProvider _authProvider = AuthProvider();
 
   Future<void> createGroup(
       BuildContext context, Map<String, dynamic> data) async {
     try {
-      Map<String, dynamic> response = await _groupProvider.createGroup(data);
+      final response = await _groupProvider.createGroup(data);
       if (response['status'] == 'success') {
-        // Exemplo: Navegar para a tela de detalhes do grupo
-        Navigator.pushNamed(context, '/group_details',
-            arguments: response['dados']['id']);
+        return response['dados']['id'];
       } else {
-        // Exemplo: Exibir uma mensagem de erro para o usuário
-        ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(
-            content: Text('Falha ao criar grupo: ${response['msg']}'),
-            backgroundColor: Colors.red,
-          ),
-        );
+        throw response['msg'];
       }
     } catch (error) {
-      // Exemplo: Exibir uma mensagem de erro genérica para o usuário
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(
-          content: Text('Erro ao criar grupo: $error'),
-          backgroundColor: Colors.red,
-        ),
-      );
+      throw error.toString();
     }
   }
 
   Future<List<SwitchModel>> getSwitchesInGroup(
       BuildContext context, String groupId) async {
     try {
-      Map<String, dynamic> response =
-          await _groupProvider.getSwitchesInGroup(groupId);
+      final response = await _groupProvider.getSwitchesInGroup(groupId);
       if (response['status'] == 'success') {
-        List<SwitchModel> switches =
-            []; // Converta os dados de resposta para modelos de Switch
-        // Lógica para converter dados de resposta em modelos de Switch
+        List<SwitchModel> switches = [];
         return switches;
       } else {
-        // Exemplo: Exibir uma mensagem de erro para o usuário
-        ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(
-            content:
-                Text('Falha ao obter switches do grupo: ${response['msg']}'),
-            backgroundColor: Colors.red,
-          ),
-        );
-        return [];
+        throw response['msg'];
       }
     } catch (error) {
-      // Exemplo: Exibir uma mensagem de erro genérica para o usuário
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(
-          content: Text('Erro ao obter switches do grupo: $error'),
-          backgroundColor: Colors.red,
-        ),
-      );
-      return [];
+      throw error.toString();
     }
   }
 
-  Future<List<GroupModel>> getGroups(
-      BuildContext context, String userId) async {
+  Future<List<GroupModel>> getGroups(BuildContext context) async {
     try {
-      Map<String, dynamic> response = await _groupProvider.getGroups(userId);
+      final userId = await _getUserId();
+      final response = await _groupProvider.getGroups(userId);
       if (response['status'] == 'success') {
-        List<GroupModel> groups =
-            []; // Converta os dados de resposta para modelos de Group
-        // Lógica para converter dados de resposta em modelos de Group
+        List<GroupModel> groups = [];
         return groups;
       } else {
-        // Exemplo: Exibir uma mensagem de erro para o usuário
-        ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(
-            content: Text('Falha ao obter grupos: ${response['msg']}'),
-            backgroundColor: Colors.red,
-          ),
-        );
-        return [];
+        throw response['msg'];
       }
     } catch (error) {
-      // Exemplo: Exibir uma mensagem de erro genérica para o usuário
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(
-          content: Text('Erro ao obter grupos: $error'),
-          backgroundColor: Colors.red,
-        ),
-      );
-      return [];
+      throw error.toString();
     }
   }
 
-  Future<void> checkSwitchInGroup(
-      BuildContext context, String macAddress) async {
+  Future<bool> toggleGroup(Map<String, dynamic> data) async {
     try {
-      Map<String, dynamic> response =
-          await _groupProvider.checkSwitchInGroup(macAddress);
-      if (response['status'] != 'success') {
-        // Exemplo: Exibir uma mensagem de erro para o usuário
-        ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(
-            content: Text(
-                'Falha ao verificar o switch no grupo: ${response['msg']}'),
-            backgroundColor: Colors.red,
-          ),
-        );
-      }
-    } catch (error) {
-      // Exemplo: Exibir uma mensagem de erro genérica para o usuário
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(
-          content: Text('Erro ao verificar o switch no grupo: $error'),
-          backgroundColor: Colors.red,
-        ),
-      );
-    }
-  }
-
-  Future<void> addSwitchToGroup(
-      BuildContext context, Map<String, dynamic> data) async {
-    try {
-      Map<String, dynamic> response =
-          await _groupProvider.addSwitchToGroup(data);
-      if (response['status'] != 'success') {
-        // Exemplo: Exibir uma mensagem de erro para o usuário
-        ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(
-            content:
-                Text('Falha ao adicionar switch ao grupo: ${response['msg']}'),
-            backgroundColor: Colors.red,
-          ),
-        );
-      }
-    } catch (error) {
-      // Exemplo: Exibir uma mensagem de erro genérica para o usuário
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(
-          content: Text('Erro ao adicionar switch ao grupo: $error'),
-          backgroundColor: Colors.red,
-        ),
-      );
-    }
-  }
-
-  Future<bool> toggleGroup(
-      BuildContext context, Map<String, dynamic> data, String userId) async {
-    try {
-      Map<String, dynamic> response = await _groupProvider.toggleGroup(data);
+      final response = await _groupProvider.toggleGroup(data);
       if (response['status'] == 'success') {
         return true;
       } else {
-        // Exemplo: Exibir uma mensagem de erro para o usuário
-        ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(
-            content: Text('Falha ao alternar o grupo: ${response['msg']}'),
-            backgroundColor: Colors.red,
-          ),
-        );
-        return false;
+        throw response['msg'];
       }
     } catch (error) {
-      // Exemplo: Exibir uma mensagem de erro genérica para o usuário
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(
-          content: Text('Erro ao alternar o grupo: $error'),
-          backgroundColor: Colors.red,
-        ),
-      );
-      return false;
+      throw error.toString();
     }
   }
 
-  Future<void> removeSwitchFromGroup(
-      BuildContext context, String macAddress) async {
-    try {
-      Map<String, dynamic> response =
-          await _groupProvider.removeSwitchFromGroup(macAddress);
-      if (response['status'] != 'success') {
-        // Exemplo: Exibir uma mensagem de erro para o usuário
-        ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(
-            content:
-                Text('Falha ao remover switch do grupo: ${response['msg']}'),
-            backgroundColor: Colors.red,
-          ),
-        );
-      }
-    } catch (error) {
-      // Exemplo: Exibir uma mensagem de erro genérica para o usuário
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(
-          content: Text('Erro ao remover switch do grupo: $error'),
-          backgroundColor: Colors.red,
-        ),
-      );
+  Future<String> _getUserId() async {
+    final userId = _authProvider.userId;
+    if (userId == null) {
+      throw 'Usuário não autenticado';
     }
-  }
-
-  Future<void> updateGroupInfo(
-      BuildContext context, Map<String, dynamic> data) async {
-    try {
-      Map<String, dynamic> response =
-          await _groupProvider.updateGroupInfo(data);
-      if (response['status'] == 'success') {
-        // Exemplo: Exibir uma mensagem de sucesso para o usuário
-        ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(
-            content: Text('Informações do grupo atualizadas com sucesso'),
-            backgroundColor: Colors.green,
-          ),
-        );
-      } else {
-        // Exemplo: Exibir uma mensagem de erro para o usuário
-        ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(
-            content: Text(
-                'Falha ao atualizar informações do grupo: ${response['msg']}'),
-            backgroundColor: Colors.red,
-          ),
-        );
-      }
-    } catch (error) {
-      // Exemplo: Exibir uma mensagem de erro genérica para o usuário
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(
-          content: Text('Erro ao atualizar informações do grupo: $error'),
-          backgroundColor: Colors.red,
-        ),
-      );
-    }
-  }
-
-  Future<void> deleteGroup(BuildContext context, String groupId) async {
-    try {
-      Map<String, dynamic> response = await _groupProvider.deleteGroup(groupId);
-      if (response['status'] == 'success') {
-        // Exemplo: Exibir uma mensagem de sucesso para o usuário
-        ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(
-            content: Text('Grupo excluído com sucesso'),
-            backgroundColor: Colors.green,
-          ),
-        );
-      } else {
-        // Exemplo: Exibir uma mensagem de erro para o usuário
-        ScaffoldMessenger.of(context).showSnackBar(
-          SnackBar(
-            content: Text('Falha ao excluir o grupo: ${response['msg']}'),
-            backgroundColor: Colors.red,
-          ),
-        );
-      }
-    } catch (error) {
-      // Exemplo: Exibir uma mensagem de erro genérica para o usuário
-      ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(
-          content: Text('Erro ao excluir o grupo: $error'),
-          backgroundColor: Colors.red,
-        ),
-      );
-    }
+    return userId;
   }
 }
