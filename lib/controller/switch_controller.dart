df264d2e1549d767b2a1dbbae7b08504928591db
jacrifla <jaquelini_cris@hotmail.com>
2024-06-01 14:09:41 -0300

    refatorado

--- lib/controller/switch_controller.dart
+++ lib/controller/switch_controller.dart
@@ -1,14 +1,16 @@
 import 'package:flutter/material.dart';
-
 import '../model/switch_model.dart';
 import '../provider/switch_provider.dart';
+import '../utils/auth_provider.dart';
 
 class SwitchController extends ChangeNotifier {
   final SwitchProvider _switchProvider = SwitchProvider();
+  final AuthProvider _authProvider = AuthProvider();
 
   Future<bool> createSwitch(
-      String name, String watts, String macAddress, String userId) async {
+      String name, String watts, String macAddress) async {
     try {
+      final userId = await _getUserId();
       final result =
           await _switchProvider.createSwitch(name, watts, macAddress, userId);
 
@@ -18,9 +20,13 @@ class SwitchController extends ChangeNotifier {
     }
   }
 
-  Future<List<SwitchModel>> getSwitches(String userId) async {
+  Future<List<SwitchModel>> getSwitches() async {
     try {
-      return await _switchProvider.getSwitches(userId);
+      final userId = await _getUserId();
+      final switches = await _switchProvider.getSwitches(userId);
+      // Notifica os ouvintes sobre a atualização dos switches
+      notifyListeners();
+      return switches;
     } catch (error) {
       throw 'Erro ao obter switches: $error';
     }
@@ -28,8 +34,7 @@ class SwitchController extends ChangeNotifier {
 
   Future<bool> updateSwitch(Map<String, dynamic> data) async {
     try {
-      final result = await _switchProvider.updateSwitch(data);
-      return result;
+      return await _switchProvider.updateSwitch(data);
     } catch (error) {
       return false;
     }
@@ -44,14 +49,22 @@ class SwitchController extends ChangeNotifier {
     }
   }
 
-  Future<bool> toggleSwitch(
-      String macAddress, bool isActive, String userId) async {
+  Future<bool> toggleSwitch(String macAddress, bool isActive) async {
     try {
+      final userId = await _getUserId();
       final result =
           await _switchProvider.toggleSwitch(macAddress, isActive, userId);
       return result['status'] == 'success';
     } catch (error) {
       return false;
     }
+  }
+
+  Future<String> _getUserId() async {
+    final userId = _authProvider.userId;
+    if (userId == null) {
+      throw 'Usuário não autenticado';
+    }
+    return userId;
   }
 }
