1c8ac5b3e1268cee8015763ad3b6e44835716ed1
jacrifla <jaquelini_cris@hotmail.com>
2024-06-01 11:45:32 -0300

    alterado

--- lib/utils/providers.dart
+++ lib/utils/providers.dart
@@ -3,20 +3,26 @@ import 'package:provider/provider.dart';
 
 import '../controller/group_controller.dart';
 import '../controller/switch_controller.dart';
+import '../controller/user_controller.dart';
 import '../provider/switch_provider.dart';
-import '../controller/perfil_controller.dart';
+import '../provider/user_provider.dart';
 import 'auth_provider.dart';
 
 class MyAppProviders extends StatelessWidget {
   final Widget child;
 
-  const MyAppProviders({required this.child, super.key});
+  const MyAppProviders({required this.child, Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
-        ChangeNotifierProvider<UserController>(create: (_) => UserController()),
+        ChangeNotifierProvider<UserController>(
+          create: (_) => UserController(
+            UserProvider(AuthProvider()),
+            AuthProvider(),
+          ),
+        ),
         ChangeNotifierProvider<AuthProvider>(create: (_) => AuthProvider()),
         ChangeNotifierProvider<SwitchProvider>(create: (_) => SwitchProvider()),
         ChangeNotifierProvider<SwitchController>(
