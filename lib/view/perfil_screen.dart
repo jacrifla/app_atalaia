41e793d1df42121e42061439e8e11f4db400fdcd
jacrifla <jaquelini_cris@hotmail.com>
2024-06-01 11:56:35 -0300

    refatorado cod

--- lib/view/perfil_screen.dart
+++ lib/view/perfil_screen.dart
@@ -1,15 +1,13 @@
-// ignore_for_file: avoid_print, use_build_context_synchronously
-
+import 'package:app_atalaia/utils/routes.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-import '../utils/auth_provider.dart';
-import 'confirmation_screen.dart';
-import '../controller/perfil_controller.dart';
+import '../controller/user_controller.dart';
 
 import '../widgets/header.dart';
 import '../widgets/menu.dart';
 import '../widgets/build_input.dart';
 import '../widgets/button_icon.dart';
+import 'confirmation_screen.dart';
 import 'success_screen.dart';
 import 'error_screen.dart';
 
@@ -32,7 +30,7 @@ class _PerfilScreenState extends State<PerfilScreen> {
         builder: (context) => ConfirmationScreen(
           question: 'Tem certeza de que deseja excluir sua conta?',
           onConfirm: () async {
-            Navigator.pushNamed(context, '/perfil');
+            Navigator.pushNamed(context, AppRoutes.userProfile);
             _excluirContaConfirmed();
           },
           onCancel: () {
@@ -45,50 +43,22 @@ class _PerfilScreenState extends State<PerfilScreen> {
 
   void _excluirContaConfirmed() async {
     final userController = Provider.of<UserController>(context, listen: false);
-    final authProvider = Provider.of<AuthProvider>(context, listen: false);
-    final userId = authProvider.userId;
-
-    if (userId == null) {
+    final success = await userController.softDeleteUser();
+    if (success) {
       Navigator.of(context).pushReplacement(
         MaterialPageRoute(
-          builder: (context) => const ErrorScreen(
-            message: 'Erro',
-            errorDescription: 'UUID do usuário não encontrado',
+          builder: (context) => const SuccessScreen(
+            message: 'Conta excluída com sucesso',
+            alternativeRoute: '/',
           ),
         ),
       );
-      return;
-    }
-
-    try {
-      bool success = await userController.softDeleteUser(userId);
-      print(success);
-      print('USER ID PERFIL: $userId');
-      if (success) {
-        Navigator.of(context).pushReplacement(
-          MaterialPageRoute(
-            builder: (context) => const SuccessScreen(
-              message: 'Conta excluída com sucesso',
-              alternativeRoute: '/',
-            ),
-          ),
-        );
-      } else {
-        Navigator.of(context).pushReplacement(
-          MaterialPageRoute(
-            builder: (context) => const ErrorScreen(
-              message: 'Erro ao excluir conta',
-              errorDescription: 'Falha ao excluir a conta',
-            ),
-          ),
-        );
-      }
-    } catch (error) {
+    } else {
       Navigator.of(context).pushReplacement(
         MaterialPageRoute(
-          builder: (context) => ErrorScreen(
+          builder: (context) => const ErrorScreen(
             message: 'Erro ao excluir conta',
-            errorDescription: error.toString(),
+            errorDescription: 'Falha ao excluir a conta',
           ),
         ),
       );
@@ -96,47 +66,42 @@ class _PerfilScreenState extends State<PerfilScreen> {
   }
 
   void _confirmarAlteracoes() async {
-    if (_formState.currentState!.validate()) {
-      final userController =
-          Provider.of<UserController>(context, listen: false);
-      final authProvider = Provider.of<AuthProvider>(context, listen: false);
-      final name = _inputNome.text;
-      final userId = authProvider.userId;
-      final email = _inputEmail.text;
-      final phone = _inputCel.text;
+    final userController = Provider.of<UserController>(context, listen: false);
 
-      if (userId == null) {
-        Navigator.of(context).pushReplacement(
-          MaterialPageRoute(
-            builder: (context) => const ErrorScreen(
-              message: 'Erro',
-              errorDescription: 'UUID do usuário não encontrado',
-            ),
-          ),
-        );
-        return;
-      }
+    final name = _inputNome.text.isNotEmpty ? _inputNome.text : null;
+    final email = _inputEmail.text.isNotEmpty ? _inputEmail.text : null;
+    final phone = _inputCel.text.isNotEmpty ? _inputCel.text : null;
 
-      try {
-        await userController.updateUserInfo(name, userId, email, phone);
-        Navigator.of(context).pushReplacement(
-          MaterialPageRoute(
-            builder: (context) => const SuccessScreen(
-              message: 'Perfil alterado com sucesso',
-              alternativeRoute: '/perfil',
-            ),
+    if (name == null && email == null && phone == null) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text('Nenhuma alteração para salvar')),
+      );
+      return;
+    }
+
+    try {
+      await userController.updateUserInfo(
+        name: name,
+        email: email,
+        phone: phone,
+      );
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(
+          builder: (context) => const SuccessScreen(
+            message: 'Perfil alterado com sucesso',
+            alternativeRoute: AppRoutes.userProfile,
           ),
-        );
-      } catch (e) {
-        Navigator.of(context).pushReplacement(
-          MaterialPageRoute(
-            builder: (context) => ErrorScreen(
-              message: 'Erro ao atualizar perfil',
-              errorDescription: e.toString(),
-            ),
+        ),
+      );
+    } catch (e) {
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(
+          builder: (context) => ErrorScreen(
+            message: 'Erro ao atualizar perfil',
+            errorDescription: e.toString(),
           ),
-        );
-      }
+        ),
+      );
     }
   }
 
@@ -170,16 +135,25 @@ class _PerfilScreenState extends State<PerfilScreen> {
                           BuildInput(
                             labelText: 'Nome',
                             controller: _inputNome,
+                            validator: (value) {
+                              return null;
+                            },
                           ),
                           BuildInput(
                             labelText: 'E-mail',
                             icon: const Icon(Icons.email_outlined),
                             controller: _inputEmail,
+                            validator: (value) {
+                              return null;
+                            },
                           ),
                           BuildInput(
                             labelText: 'Celular',
                             icon: const Icon(Icons.phone_outlined),
                             controller: _inputCel,
+                            validator: (value) {
+                              return null;
+                            },
                           ),
                         ],
                       ),
@@ -192,10 +166,8 @@ class _PerfilScreenState extends State<PerfilScreen> {
                               Theme.of(context).colorScheme.onSecondary,
                           labelText: 'Mudar senha',
                           onPressed: () {
-                            if (_formState.currentState!.validate()) {
-                              Navigator.pushNamed(
-                                  context, '/recover_confirmation');
-                            }
+                            Navigator.pushNamed(
+                                context, '/recover_confirmation');
                           },
                         ),
                         ButtonIcon(
