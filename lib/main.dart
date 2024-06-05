4a3722024116f8ce6d3eaf8b4edcfda30d904588
jacrifla <jaquelini_cris@hotmail.com>
2024-06-05 11:41:06 -0300

    tirado codigos nao utilizados

--- lib/main.dart
+++ lib/main.dart
@@ -2,15 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:get_storage/get_storage.dart';
 
 import 'app.dart';
-import 'utils/providers.dart';
 
 Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
   await GetStorage.init();
 
   runApp(
-    const MyAppProviders(
-      child: MyApp(),
-    ),
+    const MyApp(),
   );
 }
