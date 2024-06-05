3575cdbe07c8e45bbdca7991bf353299627c4664
jacrifla <jaquelini_cris@hotmail.com>
2024-06-05 14:47:28 -0300

    add novas rotas

--- lib/utils/routes.dart
+++ lib/utils/routes.dart
@@ -1,11 +1,19 @@
 import 'package:flutter/material.dart';
+
+import '../model/group_model.dart';
+import '../model/switch_model.dart';
 import '../view/group_create_screen.dart';
+import '../view/group_edit_screen.dart';
 import '../view/group_screen.dart';
 import '../view/guard_management_screen.dart';
 import '../view/help_screen.dart';
 import '../view/home_screen.dart';
 import '../view/loading_screen.dart';
+import '../view/monitor_switch_screen.dart';
+import '../view/recover_confirmation_screen.dart';
+import '../view/recover_screen.dart';
 import '../view/switch_create_screen.dart';
+import '../view/switch_edit_screen.dart';
 import '../view/switch_screen.dart';
 import '../view/login_screen.dart';
 import '../view/perfil_screen.dart';
@@ -15,42 +23,47 @@ import '../view/signup_screen.dart';
 // import '../view/recover_screen.dart';
 
 class AppRoutes {
-  static const String home = '/home';
-  static const String login = '/';
-  static const String signUp = '/register';
-  static const String userProfile = '/profile';
-  static const String help = '/help';
-  static const String loading = '/loading';
-  static const String groupScreen = '/group_screen';
-  static const String groupCreate = '/group_create';
+  static const String errorScreen = '/error_screen';
+  static const String confirmScreen = '/confirm_screen';
+  static const String guard = '/guard';
   static const String groupEdit = '/group_edit';
+  static const String groupCreate = '/group_create';
+  static const String groupScreen = '/group_screen';
+  static const String help = '/help';
+  static const String home = '/home';
+  static const String loading = '/loading';
+  static const String login = '/';
+  static const String monitorSwitch = '/monitor_switch';
+  static const String recover = '/recover';
+  static const String recoverConfirmation = '/recover_confirmation';
+  static const String signUp = '/register';
+  static const String successScreen = '/success_screen';
   static const String switchScreen = '/switch_screen';
   static const String switchCreate = '/switch_create';
   static const String switchEdit = '/switch_edit';
-  static const String guard = '/guard';
-  static const String errorScreen = '/error_screen';
-  static const String successScreen = '/success_screen';
-  static const String confirmScreen = '/confirm_screen';
-  static const String recover = '/recover';
+  static const String userProfile = '/profile';
 
   static Map<String, WidgetBuilder> getRoutes() {
     return {
-      home: (context) => const HomeScreen(),
-      login: (context) => const LoginScreen(),
-      signUp: (context) => const SignupScreen(),
-      help: (context) => const HelpScreen(),
-      userProfile: (context) => const PerfilScreen(),
-      loading: (context) => const LoadingScreen(),
-      groupScreen: (context) => const GroupScreen(),
-      groupCreate: (context) => const CreateGroupScreen(),
-      switchScreen: (context) => const SwitchScreen(),
-      switchCreate: (context) => SwitchCreateScreen(),
-      guard: (context) => GuardManagementScreen(),
-      // switchEdit: (context) => EditSwitchScreen(),
-      // groupEdit: (context) => EditGroupScreen(),
       // errorScreen: (context) => const ErrorScreen(),
-      // successScreen: (context) => const SuccessScreen(),
       // confirmScreen: (context) => const ConfirmationScreen(),
+      guard: (context) => GuardManagementScreen(),
+      groupEdit: (context) => EditGroupScreen(groupInfo: GroupModel()),
+      groupCreate: (context) => const CreateGroupScreen(),
+      groupScreen: (context) => const GroupScreen(),
+      help: (context) => const HelpScreen(),
+      home: (context) => const HomeScreen(),
+      loading: (context) => const LoadingScreen(),
+      login: (context) => const LoginScreen(),
+      monitorSwitch: (context) => const MonitorSwitchScreen(),
+      recover: (context) => const RecoverScreen(),
+      recoverConfirmation: (context) => const RecoverConfirmationScreen(),
+      signUp: (context) => const SignupScreen(),
+      // successScreen: (context) => const SuccessScreen(),
+      switchScreen: (context) => const SwitchScreen(),
+      switchCreate: (context) => const SwitchCreateScreen(),
+      switchEdit: (context) => EditSwitchScreen(switchModel: SwitchModel()),
+      userProfile: (context) => const PerfilScreen(),
     };
   }
 }
