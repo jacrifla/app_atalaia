c5dede6efaf8ebd2f14c5e98e7812207b5a61a34
jacrifla <jaquelini_cris@hotmail.com>
2024-06-08 02:05:54 -0300

    arrumado

--- lib/model/group_model.dart
+++ lib/model/group_model.dart
@@ -8,8 +8,7 @@ class GroupModel {
   String? scheduleStart;
   String? scheduleEnd;
   String? userId;
-  String? macAddress;
-  String? keepFor;
+  List<String>? macAddresses;
 
   GroupModel({
     this.groupId,
@@ -19,20 +18,23 @@ class GroupModel {
     this.scheduleStart,
     this.scheduleEnd,
     this.userId,
-    this.macAddress,
-    this.keepFor,
+    this.macAddresses,
   });
 
   GroupModel.fromJson(Map<String, dynamic> json) {
     groupId = json['uuid'];
     groupName = json['name'];
-    isActive = json['is_active'] == 1;
-    scheduleActive = json['schedule_active'] == 1;
+    isActive = json.containsKey('is_active') ? json['is_active'] == 1 : false;
+    scheduleActive = json.containsKey('schedule_active')
+        ? json['schedule_active'] == 1
+        : false;
     scheduleStart = json['schedule_start'];
     scheduleEnd = json['schedule_end'];
     userId = json['user_id'];
-    macAddress = json['mac_address'];
-    keepFor = json['keep_for'];
+    macAddresses =
+        json.containsKey('mac_address') && json['mac_address'] != null
+            ? [json['mac_address']]
+            : [];
   }
 
   Map<String, dynamic> toJson() {
@@ -44,8 +46,7 @@ class GroupModel {
     data['schedule_start'] = scheduleStart;
     data['schedule_end'] = scheduleEnd;
     data['user_id'] = userId;
-    data['mac_address'] = macAddress;
-    data['keep_for'] = keepFor;
+    data['mac_address'] = macAddresses;
     return data;
   }
 
@@ -55,6 +56,33 @@ class GroupModel {
 
   @override
   String toString() {
-    return 'GroupModel(groupId: $groupId, groupName: $groupName, isActive: $isActive, scheduleActive: $scheduleActive, scheduleStart: $scheduleStart, scheduleEnd: $scheduleEnd, userId: $userId, macAddress: $macAddress)';
+    return 'GroupModel(group_id: $groupId, groupName: $groupName, isActive: $isActive, scheduleActive: $scheduleActive, scheduleStart: $scheduleStart, scheduleEnd: $scheduleEnd, user_id: $userId, macAddresses: $macAddresses)';
+  }
+
+  factory GroupModel.fromJsonMap(Map<String, dynamic> json) {
+    // Verifica se as chaves necessárias estão presentes no objeto json
+    if (json.containsKey('uuid') &&
+        json.containsKey('name') &&
+        json.containsKey('is_active') &&
+        json.containsKey('schedule_active') &&
+        json.containsKey('schedule_start') &&
+        json.containsKey('schedule_end') &&
+        json.containsKey('user_id')) {
+      return GroupModel(
+        groupId: json['uuid'],
+        groupName: json['name'],
+        isActive: json['is_active'] == 1,
+        scheduleActive: json['schedule_active'] == 1,
+        scheduleStart: json['schedule_start'],
+        scheduleEnd: json['schedule_end'],
+        userId: json['user_id'],
+        macAddresses: [json['mac_address'] ?? ''],
+      );
+    } else {
+      // Se alguma chave estiver ausente, imprime uma mensagem de erro
+      print('Erro: Algumas chaves estão ausentes no objeto json');
+      // Retorna um novo GroupModel com valores nulos
+      return GroupModel();
+    }
   }
 }
