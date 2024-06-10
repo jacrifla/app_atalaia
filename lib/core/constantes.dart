c6bf00adbcb9bf94d21e9f05cfa753c0576ad4e9
jacrifla <jaquelini_cris@hotmail.com>
2024-06-10 04:06:32 -0300

    mudado de pasta

--- lib/core/constantes.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-
-// Constantes para as cores alternadas
-
-const Color activeColor = Color.fromARGB(255, 196, 196, 12);
-const Color inactiveColor = Color.fromARGB(255, 141, 141, 141);
---
+++ lib/utils/constantes.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+// Constantes para as cores alternadas
+
+const Color activeColor = Color(0xFFFFCC24);
+const Color inactiveColor = Color(0xFF8D8D8D);
+const Color success = Color.fromARGB(255, 8, 124, 37);
+const Color edit = Color.fromARGB(255, 5, 131, 189);
+const Color delete = Color(0xFF7E0000);
