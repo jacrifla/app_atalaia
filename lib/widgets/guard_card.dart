cad66596344415e225d5d631d07bd1f6640e1666
jacrifla <jaquelini_cris@hotmail.com>
2024-06-13 22:21:09 -0300

    fix card

--- lib/widgets/guard_card.dart
+++ lib/widgets/guard_card.dart
@@ -38,50 +38,42 @@ class _GuardCardState extends State<GuardCard> {
                 color: _guardaAtiva ? ativate : desativate,
               ),
             ),
-            Column(
-              children: [
-                SizedBox(
-                  height: 50,
-                  child: Text(
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
                     'Guarda'.toUpperCase(),
                     style: const TextStyle(
                       fontSize: 25,
                       fontWeight: FontWeight.bold,
                     ),
                   ),
-                ),
-                Column(
-                  children: [
-                    SizedBox(
-                      width: 250,
-                      child: Text(
-                        _guardaAtiva
-                            ? 'Sua guarda está ativa no momento.'
-                            : 'Sua guarda não está ativa no momento.',
-                        style: TextStyle(
-                          color: _guardaAtiva ? ativate : desativate,
-                        ),
-                        textAlign: TextAlign.center,
-                      ),
+                  const SizedBox(height: 10),
+                  Text(
+                    _guardaAtiva
+                        ? 'Sua guarda está ativa no momento.'
+                        : 'Sua guarda não está ativa no momento.',
+                    style: TextStyle(
+                      color: _guardaAtiva ? ativate : desativate,
                     ),
-                    const SizedBox(height: 20),
-                    ElevatedButton(
-                      onPressed: _toggleGuarda,
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor:
-                            !_guardaAtiva ? ativate : guardaButtonColor,
-                        minimumSize: const Size(200, 40),
-                      ),
-                      child: Text(
-                        _guardaAtiva
-                            ? 'Desativar Guarda'
-                            : 'Ativar Guarda Agora',
-                      ),
+                    textAlign: TextAlign.start,
+                  ),
+                  const SizedBox(height: 20),
+                  ElevatedButton(
+                    onPressed: _toggleGuarda,
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor:
+                          !_guardaAtiva ? ativate : guardaButtonColor,
+                      minimumSize: const Size(200, 40),
                     ),
-                  ],
-                )
-              ],
-            )
+                    child: Text(
+                      _guardaAtiva ? 'Desativar Guarda' : 'Ativar Guarda Agora',
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       ),
