303b77834ba10069a6d93e07bcdab57f1a95c782
jacrifla <jaquelini_cris@hotmail.com>
2024-06-14 12:23:39 -0300

    fix groupmodel

--- lib/model/group_model.dart
+++ lib/model/group_model.dart
@@ -60,29 +60,16 @@ class GroupModel {
   }
 
   factory GroupModel.fromJsonMap(Map<String, dynamic> json) {
-    // Verifica se as chaves necessárias estão presentes no objeto json
-    if (json.containsKey('uuid') &&
-        json.containsKey('name') &&
-        json.containsKey('is_active') &&
-        json.containsKey('schedule_active') &&
-        json.containsKey('schedule_start') &&
-        json.containsKey('schedule_end') &&
-        json.containsKey('user_id')) {
-      return GroupModel(
-        groupId: json['uuid'],
-        groupName: json['name'],
-        isActive: json['is_active'] == 1,
-        scheduleActive: json['schedule_active'] == 1,
-        scheduleStart: json['schedule_start'],
-        scheduleEnd: json['schedule_end'],
-        userId: json['user_id'],
-        macAddresses: [json['mac_address'] ?? ''],
-      );
-    } else {
-      // Se alguma chave estiver ausente, imprime uma mensagem de erro
-      print('Erro: Algumas chaves estão ausentes no objeto json');
-      // Retorna um novo GroupModel com valores nulos
-      return GroupModel();
-    }
+    return GroupModel(
+      groupId: json['uuid'] as String?,
+      groupName: json['name'] as String?,
+      isActive: json['is_active'] == 1,
+      scheduleActive: json['schedule_active'] == 1,
+      scheduleStart: json['schedule_start'] as String?,
+      scheduleEnd: json['schedule_end'] as String?,
+      userId: json['user_id'] as String?,
+      macAddresses:
+          json['mac_address'] != null ? [json['mac_address'] as String] : [],
+    );
   }
 }
