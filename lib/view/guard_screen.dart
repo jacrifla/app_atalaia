49c24bfbd9b1bc61cf208d309a74b8360e7d2f86
jacrifla <jaquelini_cris@hotmail.com>
2024-06-14 23:29:08 -0300

    aparecendo os pontos

---
+++ lib/view/guard_screen.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import '../controller/guard_controller.dart';
+import '../provider/guard_provider.dart';
+import '../model/switch_model.dart';
+import '../widgets/header.dart';
+import '../widgets/menu.dart';
+import '../widgets/guard_card_toggle.dart';
+
+class GuardScreen extends StatefulWidget {
+  const GuardScreen({super.key});
+
+  @override
+  State<GuardScreen> createState() => _GuardScreenState();
+}
+
+class _GuardScreenState extends State<GuardScreen> {
+  final GuardProvider guardProvider = GuardProvider();
+  late final GuardController ctlGuardController;
+  final List<SwitchModel> guardSwitches = [];
+
+  @override
+  void initState() {
+    super.initState();
+    ctlGuardController = GuardController(provider: guardProvider);
+    _loadGuardSwitches();
+  }
+
+  Future<void> _loadGuardSwitches() async {
+    bool success = await ctlGuardController.getGuardInfo();
+    if (success && ctlGuardController.guardInfo != null) {
+      setState(() {
+        guardSwitches.clear();
+        guardSwitches.addAll(ctlGuardController.guardInfo!.switches ?? []);
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: const Header(title: 'Ger. Guarda'),
+      endDrawer: const MenuDrawer(),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: guardSwitches.isEmpty
+            ? const Center(child: CircularProgressIndicator())
+            : ListView.builder(
+                itemCount: guardSwitches.length,
+                itemBuilder: (context, index) {
+                  return GuardCardToggle(switchModel: guardSwitches[index]);
+                },
+              ),
+      ),
+    );
+  }
+}
