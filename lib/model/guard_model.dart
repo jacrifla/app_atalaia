9a4846c4a9dbc34c4b1464ae0be0eb6b0f033e49
jacrifla <jaquelini_cris@hotmail.com>
2024-06-14 23:31:48 -0300

    guardmodel

--- lib/model/guard_model.dart
+++ lib/model/guard_model.dart
@@ -1,21 +1,40 @@
+import 'switch_model.dart';
+
 class GuardModel {
   String? uuid;
-  String? userId;
-  String? isActive;
+  List<SwitchModel>? switches;
 
-  GuardModel({this.uuid, this.userId, this.isActive});
+  GuardModel({this.uuid, this.switches});
 
-  GuardModel.fromJson(Map<String, dynamic> json) {
-    uuid = json['uuid'];
-    userId = json['user_id'];
-    isActive = json['is_active'];
+  GuardModel.fromJson(List<dynamic> jsonList) {
+    if (jsonList.isNotEmpty) {
+      uuid = jsonList[0]['guard_id'];
+      switches = jsonList
+          .map((item) => SwitchModel.fromJson({
+                'uuid': item['guard_id'],
+                'user_id': null,
+                'group_id': null,
+                'mac_address': item['mac_address'],
+                'name': item['switch_name'],
+                'is_active': null,
+                'watts': null,
+                'guard_active': item['guard_active'],
+              }))
+          .toList();
+    }
   }
 
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = <String, dynamic>{};
-    data['uuid'] = uuid;
-    data['user_id'] = userId;
-    data['is_active'] = isActive;
+    data['guard_id'] = uuid;
+    if (switches != null) {
+      data['switches'] = switches!.map((v) => v.toJson()).toList();
+    }
     return data;
+  }
+
+  @override
+  String toString() {
+    return 'GuardModel(uuid: $uuid, switches: $switches)';
   }
 }
