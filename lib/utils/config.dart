87bebcd098a8393fbab885c5b4cf8af5fa5c91f6
jacrifla <jaquelini_cris@hotmail.com>
2024-06-22 22:59:07 -0300

    adds configs novas

--- lib/utils/config.dart
+++ lib/utils/config.dart
@@ -6,10 +6,16 @@ class Config {
   // static const String apiUrl = 'http://192.168.101.7:80/app_atalaia/api2';
 
   // IP Fixo Fatec
-  static const String apiUrl = 'http://192.168.1.100:80/app_atalaia/api2';
+  // static const String apiUrl = 'http://192.168.181.144:80/app_atalaia/api2';
+
+  // CUSTOMIZE
+  // static const String apiUrl = 'http://192.168.1.52:80/app_atalaia/api2';
+
+  // CEL
+  // static const String apiUrl = 'http://192.168.248.144:80/app_atalaia/api2';
 
   // NOTE
-  // static const String apiUrl = 'http://192.168.1.29:80/app_atalaia/api2';
+  static const String apiUrl = 'http://192.168.1.29:80/app_atalaia/api2';
 
   // REMOTE SERVER (TEMP)
   // static const String apiUrl = 'https://atalaiaproject.000webhostapp.com/';
