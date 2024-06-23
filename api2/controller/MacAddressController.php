493b4d84037f027f87f99821bee2c50f12e03de3
Sthefani Souza <slauraic@hotmail.com>
2024-06-22 22:49:13 -0300

    Recuperando classes

---
+++ api2/controller/MacAddressController.php
@@ -0,0 +1,94 @@
+<?php
+
+require_once './core/Request.php';
+require_once './core/Response.php';
+require_once './model/MacModel.php';
+//somente uso interno
+class MacAddressController
+{
+
+    public function insert(Request $request, Response $response)
+    {
+        try {
+            $data = $request->bodyJson();
+            
+            $success = MacModel::insert($data['mac_address']);
+                
+                if ($success) {     
+
+                    $response::json([
+                        'status' => 'success',
+                        'dados' => $data
+                    ], 201);
+                }else{
+                        $response::json([
+                            'status' => 'error',
+                            'msg' => 'Unkwon error'
+                        ], 400);
+                }
+            } catch (\Exception $e) {
+                $response::json([
+                    'status' => 'error',
+                    'msg' => $e->getMessage()
+                ], 500);
+            }
+        }
+
+        public function update(Request $request, Response $response)
+    {
+        try {
+            $data = $request->bodyJson();
+            
+            $success = MacModel::update($data['id'],$data['mac_address']);
+                
+                if ($success) {     
+
+                    $response::json([
+                        'status' => 'success',
+                        'dados' => $data
+                    ], 201);
+                }else{
+                        $response::json([
+                            'status' => 'error',
+                            'msg' => 'Unkwon error'
+                        ], 400);
+                }
+            } catch (\Exception $e) {
+                $response::json([
+                    'status' => 'error',
+                    'msg' => $e->getMessage()
+                ], 500);
+            }
+        }
+
+        public function delete(Request $request, Response $response)
+    {
+        try {
+            $data = $request->bodyJson();
+            
+            $success = MacModel::delete($data['id']);
+                
+                if ($success) {     
+
+                    $response::json([
+                        'status' => 'success',
+                        'dados' => $data
+                    ], 201);
+                }else{
+                        $response::json([
+                            'status' => 'error',
+                            'msg' => 'Unkwon error'
+                        ], 400);
+                }
+            } catch (\Exception $e) {
+                $response::json([
+                    'status' => 'error',
+                    'msg' => $e->getMessage()
+                ], 500);
+            }
+        }
+        
+        
+        
+    }
+    
---
+++ api2/model/MacModel.php
@@ -0,0 +1,113 @@
+<?php
+
+require_once './core/ConnectionMYSQL.php';
+require_once './core/ExceptionPdo.php';
+
+class MacModel
+{
+    //checa se o endereço mac enviado pelo usuario existe na base de mac disponiveis ou se nao está em uso (serve para caso o mac seja digitado erroneamente)
+    public static function checkMacAddressAvailable($mac_address){
+        try {
+            $pdo = ConnectionMYSQL::getInstance();
+    
+    
+            $stmt = $pdo->prepare('
+                SELECT id, is_used 
+                FROM tb_mac_address 
+                WHERE mac_address = ? '
+            );
+            
+            $stmt->execute([$mac_address]);
+    
+            $mac_record = $stmt->fetch(PDO::FETCH_ASSOC);
+
+            if(($stmt->rowCount() <= 0) || $mac_record['is_used'] ){
+                return null;
+            }
+
+            return $mac_record;
+        } catch (\PDOException $e) {
+            throw new \Exception(ExceptionPdo::translateError($e->getMessage()));
+        } catch (\Exception $e) {
+            throw new \Exception($e->getMessage());
+        }
+    }
+
+    //Quando um switch é criado atualiza na tabela de mac addresses que esse mac foi colocado em uso por algum usuario
+    public static function updateMacAddressRecord($mac_address)
+    {
+        try {
+            $pdo = ConnectionMYSQL::getInstance();
+
+            $stmt = $pdo->prepare('UPDATE tb_mac_address SET is_used = true WHERE mac_address = ?');
+            $stmt->execute([$mac_address]);
+
+            return ($stmt->rowCount() > 0);
+        } catch (\PDOException $e) {
+            throw new \Exception(ExceptionPdo::translateError($e->getMessage()));
+        } catch (\Exception $e) {
+            throw new \Exception($e->getMessage());
+        }
+    }
+
+    //USO INTERNO
+    public static function insert($mac_address){
+        try {
+            $pdo = ConnectionMYSQL::getInstance();
+
+            $stmt = $pdo->prepare('
+            INSERT INTO tb_mac_address (mac_address) 
+            VALUES (?)'
+        );
+
+        $stmt->execute([$mac_address]);
+
+            return ($stmt->rowCount() > 0);
+        } catch (\PDOException $e) {
+            throw new \Exception(ExceptionPdo::translateError($e->getMessage()));
+        } catch (\Exception $e) {
+            throw new \Exception($e->getMessage());
+        }
+    }
+
+    //USO INTERNO
+    public static function delete($id){
+        try {
+            $pdo = ConnectionMYSQL::getInstance();
+    
+            $stmt = $pdo->prepare('
+                DELETE FROM tb_mac_address 
+                WHERE id = ?
+            ');
+    
+            $stmt->execute([$id]);
+    
+            return ($stmt->rowCount() > 0);
+        } catch (\PDOException $e) {
+            throw new \Exception(ExceptionPdo::translateError($e->getMessage()));
+        } catch (\Exception $e) {
+            throw new \Exception($e->getMessage());
+        }
+    }
+
+    //USO INTERNO
+    public static function update($id, $new_mac_address){
+        try {
+            $pdo = ConnectionMYSQL::getInstance();
+
+            $stmt = $pdo->prepare('
+                UPDATE tb_mac_address 
+                SET mac_address = ?
+                WHERE id = ?
+            ');
+
+            $stmt->execute([ $new_mac_address, $id]);
+
+            return ($stmt->rowCount() > 0);
+        } catch (\PDOException $e) {
+            throw new \Exception(ExceptionPdo::translateError($e->getMessage()));
+        } catch (\Exception $e) {
+            throw new \Exception($e->getMessage());
+        }
+    }
+}
